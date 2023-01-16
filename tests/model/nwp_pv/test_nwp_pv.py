717259a99f6e209df08f1894f0087df5947c25e7
peterdudfield <peter.dudfield@hotmail.com>
2023-01-16 08:31:48 +0000

    fix test

--- tests/model/nwp_pv/test_nwp_pv.py
+++ tests/model/nwp_pv/test_nwp_pv.py
@@ -1,11 +1,11 @@
-from pv_site_production.models.pv_nwp.nwp_pv import run_one_batch
+from pv_site_production.models.pv_nwp import nwp_pv
 from ocf_datapipes.batch.fake.fake_batch import make_fake_batch
 from ocf_datapipes.config.load import load_yaml_configuration
 import os
 
 
 def get_batch():
-    configuration_file = os.path.dirname(run_one_batch) + "/configuration.yaml"
+    configuration_file = os.path.dirname(nwp_pv.__file__) + "/configuration.yaml"
     configuration = load_yaml_configuration(configuration_file)
     return make_fake_batch(configuration=configuration)
 
@@ -13,6 +13,6 @@ def get_batch():
 def test_run_one_batch():
 
     batch = get_batch()
-    results = run_one_batch(batch=batch)
+    results = nwp_pv.run_one_batch(batch=batch)
 
     # TODO add asserts
