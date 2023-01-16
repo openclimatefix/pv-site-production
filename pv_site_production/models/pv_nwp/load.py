8cdf1d5d848b4576fb8a8e93a26aee5121594cf8
peterdudfield <peter.dudfield@hotmail.com>
2023-01-16 08:21:27 +0000

    load model (+tests)

---
+++ pv_site_production/models/pv_nwp/load.py
@@ -0,0 +1,6 @@
+""" File to load the model (and weights)"""
+
+
+def load_model():
+    """Load model and return it"""
+    return None
---
+++ tests/model/nwp_pv/test_nwp_pv_load.py
@@ -0,0 +1,7 @@
+from pv_site_production.models.pv_nwp.load import load_model
+
+
+def test_load_model():
+    model = load_model()
+    # TODO
+    # assert model is not None
