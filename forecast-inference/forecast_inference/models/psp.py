df8d5be5a69edb773df9041840b6257959eb2e43
peterdudfield <peter.dudfield@hotmail.com>
2025-03-19 15:01:10 +0000

    rename nwp data

--- forecast-inference/forecast_inference/models/psp.py
+++ forecast-inference/forecast_inference/models/psp.py
@@ -2,12 +2,14 @@
 Models from the `pv-site-prediction` repo.
 """
 import logging
+import os
 from typing import Any
 
 from psp.data_sources.pv import PvDataSource
 from psp.models.base import PvSiteModel
 from psp.serialization import load_model
 
+from forecast_inference.data.nwp_data_sources import download_and_add_osgb_to_nwp_data_source
 from forecast_inference.utils.imports import instantiate
 from forecast_inference.utils.profiling import profile
 
@@ -19,6 +21,10 @@ def get_model(config: dict[str, Any], pv_data_source: PvDataSource) -> PvSiteMod
 
     with profile(f'Loading model: {config["model_path"]}'):
         model = load_model(config["model_path"])
+
+    # download and add osbg to nwp datasource
+    if os.getenv("NWP_ZARR_PATH") is not None:
+        download_and_add_osgb_to_nwp_data_source(os.getenv("NWP_ZARR_PATH"), "nwp.zarr")
 
     with profile(f'Getting NWP data: {config["nwp"]}'):
         nwp_data_sources = instantiate(**config["nwp"])
